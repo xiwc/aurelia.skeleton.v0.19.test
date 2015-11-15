db3025f42683f0c00b10797fd84aa198428b67dc
weicheng xi <weicheng.xi@newtouch.cn>
2015-11-15 15:06:48 +0800

    父子路由切换同步问题验证

--- src/app.js
+++ src/app.js
@@ -1,12 +1,36 @@
 export class App {
-  configureRouter(config, router) {
-    config.title = 'Aurelia';
-    config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
-    ]);
+    configureRouter(config, router) {
+        config.title = 'Aurelia';
+        config.map([{
+            route: ['welcome'],
+            name: 'welcome',
+            moduleId: 'welcome',
+            nav: true,
+            title: 'Welcome'
+        }, {
+            route: ['parent/:id'],
+            name: 'parent',
+            moduleId: 'parent',
+            nav: false,
+            title: 'Parent',
+            href: 'parent'
+        }, {
+            route: 'users',
+            name: 'users',
+            moduleId: 'users',
+            nav: true,
+            title: 'Github Users'
+        }, {
+            route: 'child-router',
+            name: 'child-router',
+            moduleId: 'child-router',
+            nav: true,
+            title: 'Child Router'
+        }, {
+            route: '',
+            redirect: 'welcome'
+        }]);
 
-    this.router = router;
-  }
+        this.router = router;
+    }
 }
