7a604f71feb81330701a7a9490ac1409efe1393f
weicheng xi <weicheng.xi@newtouch.cn>
2015-11-15 15:06:48 +0800

    父子路由切换同步问题验证

---
+++ src/parent.js
@@ -0,0 +1,29 @@
+import {
+    activationStrategy
+}
+from 'aurelia-router';
+
+export class Parent {
+
+    determineActivationStrategy() {
+        return activationStrategy.replace;
+    }
+
+    configureRouter(config, router) {
+        config.map([{
+            route: ['', 'welcome'],
+            name: 'welcome',
+            moduleId: 'welcome',
+            nav: true,
+            title: 'Welcome'
+        }, {
+            route: 'users',
+            name: 'users',
+            moduleId: 'users',
+            nav: true,
+            title: 'Github Users'
+        }]);
+
+        this.router = router;
+    }
+}
